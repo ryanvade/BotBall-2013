8c70dcf193249ceeae244a8fabd6c63b36d2eb78
Ryan <ryanvader82@gmail.com>
2013-04-04 18:00:12 -0500

    added some customization

--- servo_basic.c
+++ servo_basic.c
@@ -1,11 +1,12 @@
-/********  Stop when accelerometer shows robot has tilted              
+/********  move the servo from A to B              
 *****************************************************************/
 int main() {
    // preset servo 1 position
-   printf("advance using A button\n\nB to quit\n");   set_servo_position(1,700); 
+   set_servo_position(1,700); 
+	printf("Press B to stop");
    enable_servos(); // turn on servos
    msleep(2000); // pause while it moves and user reads screen
-   while((accel_y() > -150) && (b_button()==0))
+   while((b_button()==0))
    {  // move servo 1 in steps of 100
       set_servo_position(1,get_servo_position(1)+ 1348);
       printf("servo at %d\n", get_servo_position(1));
@@ -13,7 +14,7 @@ int main() {
       while((!a_button()) && (!b_button())) {}
    }
    disable_servos();
-   printf("Tilt! Robot is done\n");
+   printf("Robot is done\n");
    return 0;
 }
 
