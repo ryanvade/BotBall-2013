2ebafb557e1556d99c2bfb5dfa1ca9adf05c057c
Ryan Owens <ryanvader82@gmail.com>
2013-04-16 15:38:19 -0500

    Added files to repo

--- Robot_A.c
+++ Robot_A.c
@@ -1,43 +1,48 @@
 // Created on Wenesday April 10 2013
 /* This program will move Robot A accordig to its assignment. */
+int initialize(); // tell the compiler about the initialize function
+int move(); // tell the compiler about the move function
+int grabBotGuy(); // tell the compiler about the grabBotGuy function
+int returnBotGuy();// tell the compiler about th returnBotGuy function
 
-int line_Follow( int threshold, int rport, int high, int low);
-
-int main()
-{
-	printf("Hello, World!\n");
-	create_connect(); // start the connection to the create
-	printf("Connected to the Create\n"); // tell the world what is going on. 
-	set_create_distance(0); //set distance accumulator at 0
-	set_create_total_angle(0); //set angle acuumulator at 0
-	int rport=1; // identify ports used, might need to change
-	printf("Light sensor is supposed to be port %d. ", &rport);
-	int threshold=500; // light threshold in the room.  Black is around 0, plus standard light brings the light at ~512
-	//int high=100, low=0; //motor power for arc. Will need to change to perfect drive.  * Probably not needed for Create.
-			
-	int line_Follow(int threshold, int rport, int high, int low);
-			
-	create_disconnect(); // disconnect from the create
+int main() {
+	create_conect();  
+	print(" Initializing.\n");
+	int initalize();
+	printf("Starting to move to location.\n");
+	int move();
+	printf("Grabing Bot Guy\n");
+	int grabBotGuy
+	printf("Returning Bot Guy\n");
+	create_disconnect();
 	return 0;
 }
-int line_Follow(int threshold,int rport, int high, int low)
+
+	int grabBotGuy() 
 	{
-			printf("\n The threshold is currently at %d", &threshold);
-	while (b_button()==0) {} // start after pushing B 
-	/* To be replaced with wait_for_light(port#); so that light starts the robot. Right now it is started with B*/
-	while (a_button()==0){ // stop after pushing A
-		/* to be relplaced with a button sensor. Such as digital(Port #); so that the claw grabs the transport when the sensor(s) are pushed.*/
-			while (analog10(rport) > threshold) { // continue until not dark
-			create_drive_direct(0,300); // arc left
-			printf("The current light strength is %d", analog10(rport));
-			if (a_button()!=0) break; } // or button pressed
-			// to be replaced with a sensor
-			while (analog10(rport) <= threshold){ // continue until dark
-			create_drive_direct(300,0); // arc right
-			printf("The current light strength is %d", analog10(rport));
-			if (a_button()!=0) break; } // or button pressed 
-			// to be replaced with a sensor
-			create_stop(); // stop all create motors. * not used in the create example. 
-										}
+	int triad = 0; // initialize triad to be 0
+	motor(1,-100); // move motor 1 at -100%
+	motor(3,-100); // move motor 3 at -100%
+	msleep(500); // stop moving the motors after .5 seconds
+	ao(); // stop th motors
+	while(triad == 0) {
+		if (analog10(2) < 950) {
+			
+			printf("Give me a hug! ;)");
+			motor(1,100); //Move motor 0 at full power to grip BotGuy.
+			motor(3,100); //Move motor 1 at full power to grip BotGuy.
+		}
+		else {
+			printf("Where is Bot Guy?");
+			msleep(200);
+		}
 	return 0;
+	}
+	
+	int move ()
+	{ 
+				while(analog10(3) > 500) {}// while the light is not on do nothing. When the light is on start.
+					create_drive_direct(-200, -200); //Move create backwards.
+						
+			return 0;
 	}
--- Robot_B.c
+++ Robot_B.c
@@ -1,25 +1,14 @@
-// Created on Sat April 6 2013
-/* This program will move Robot B accordig to its assignment.  First it will follow the black line to the Launchpad, grab the Launchpad, then drag it to the launch area */
+// Created on Sat April 13 2013
+// This is a transport move test. 
 int main()
 {
 	printf("Hello, World!\n");
-	int rport=1, leftmtr=3, rightmtr=0; // identify ports used, might need to change
-	int threshold=700; // light threshold in the room
-	int high=100, low=-10; //motor power for arc. Will need to change to perfect drive. 
-	while (b_button()==0) {} // start after pushing B 
-	/* To be replaced with wait_for_light(port#); so that light starts the robot. Right now it is started with B*/
-	while (a_button()==0){ // stop after pushing A
-		/* to be relplaced with a button sensor. Such as digital(Port #); so that the claw grabs the transport when the sensor(s) are pushed.*/
-			while (analog10(rport) > threshold) { // continue until not dark
-			motor(leftmtr,low); motor(rightmtr,high); // arc left
-			if (a_button()!=0) break; } // or button pressed
-			// to be replaced with a sensor
-			while (analog10(rport) <= threshold){ // continue until dark
-			motor(leftmtr,high); motor(rightmtr,low); // arc right
-			if (a_button()!=0) break; } // or button pressed 
-			// to be replaced with a sensor
-			ao(); 
-			//all stop because button was pushed. 
-		}
+	motor(0,-100);
+	motor(2,-100);
+	
+	msleep(2000);
+	
+	ao();
 	return 0;
+
 }
